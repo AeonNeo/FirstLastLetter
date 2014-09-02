c450c6c8b53df907686ec806fc6c504abf8dde2e
Voltrik <vincenttseng06@gmail.com>
2014-09-02 16:49:59 -0700

    test

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -3,6 +3,10 @@ public class FirstLastLetter {
 	public static void main(String[] args)
 	{
 		System.out.println("you're a faggot");
+		System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");
+		
 	}
+	
+
 
 }
