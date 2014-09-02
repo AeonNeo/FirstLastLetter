3f10b2a3c40886d35382ae926603950d66c6885c
Vincent Yang <VincentYang2014@gmail.com>
2014-09-02 16:48:37 -0700

    added stuff

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -3,6 +3,7 @@ public class FirstLastLetter {
 	public static void main(String[] args)
 	{
 		System.out.println("you're a faggot");
+		System.out.println("If a tree fell in the forest and nobody was around");
 	}
 
 }
