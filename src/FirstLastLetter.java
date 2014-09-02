96f7d89abf5ef5eb5b9c7fe7fc393f1da205665c
Voltrik <vincenttseng06@gmail.com>
2014-09-02 16:57:14 -0700

    hi

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -3,8 +3,13 @@ public class FirstLastLetter {
 	public static void main(String[] args)
 	{
 		System.out.println("you're a faggot");
+<<<<<<< Updated upstream
 		System.out.println("whatever changes you usedta have");
 		System.out.println("If a tree fell in the forest and nobody was around");
+=======
+		System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");
+		
+>>>>>>> Stashed changes
 
 	}
 	
