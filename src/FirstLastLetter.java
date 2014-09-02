8f8b7255a9dbb768f8bfac19d12507cacfa14b38
Vincent Yang <VincentYang2014@gmail.com>
2014-09-02 16:59:32 -0700

    new messages

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -3,14 +3,13 @@ public class FirstLastLetter {
 	public static void main(String[] args)
 	{
 		System.out.println("you're a faggot");
-<<<<<<< Updated upstream
 		System.out.println("whatever changes you usedta have");
 		System.out.println("If a tree fell in the forest and nobody was around");
-=======
-		System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");
-		
->>>>>>> Stashed changes
 
+		System.out.println("vincent's a faggot");
+		System.out.println("vincent's a faggot");
+		System.out.println("vincent's a faggot");
+		System.out.println("mins a faggot. new stuff");
 	}
 	
 
