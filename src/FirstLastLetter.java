a84d070fc31c87b93feb7d34e6800e52ea6b92e7
Vincent Yang <VincentYang2014@gmail.com>
2014-09-02 16:53:30 -0700

    made changes

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -3,12 +3,9 @@ public class FirstLastLetter {
 	public static void main(String[] args)
 	{
 		System.out.println("you're a faggot");
-<<<<<<< HEAD
-		System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");System.out.println("vincent's a fagget");
-		
-=======
+		System.out.println("whatever changes you usedta have");
 		System.out.println("If a tree fell in the forest and nobody was around");
->>>>>>> 3f10b2a3c40886d35382ae926603950d66c6885c
+
 	}
 	
 
