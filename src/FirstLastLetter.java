d182faf38465fe5f18b960effec0130d55599962
Voltrik <vincenttseng06@gmail.com>
2014-09-02 17:10:12 -0700

    hi

--- src/FirstLastLetter.java
+++ src/FirstLastLetter.java
@@ -2,6 +2,7 @@
 public class FirstLastLetter {
 	public static void main(String[] args)
 	{
+		System.out.println("hihihihhi");
 		System.out.println("you're a faggot");
 		System.out.println("whatever changes you usedta have");
 		System.out.println("If a tree fell in the forest and nobody was around");
